366d5f8e6a5c1319455d74e83047703e1667a498
Dario <darior87@yahoo.it>
2017-08-29 09:16:47 +0200

    fix readme

--- package.js
+++ package.js
@@ -1,6 +1,6 @@
 Package.describe({
   name: 'darior87:meteor-accounts-stripe-connect',
-  version: '0.0.2',
+  version: '0.0.3',
   // Brief, one-line summary of the package.
   summary: 'Login service for stripe accounts',
   // URL to the Git repository containing the source code for this package.
